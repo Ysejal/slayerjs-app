48c247e6cf7a7300b391bdc4805b28b1fa0ddad2
Soufiane Chikar <soufiane.chikar@tutamail.com>
2021-04-15 09:56:21 +0200

    edit app.js

--- src/client/App.js
+++ src/client/App.js
@@ -3,13 +3,10 @@ import './app.css';
 import ReactImage from './react.png';
 
 export default class App extends Component {
-  state = { username: null };
-
   render() {
-    const { username } = this.state;
     return (
       <div>
-        {username ? <h1>{`Slayer`}</h1> : <h1>Loading.. please wait!</h1>}
+        {<h1>{`Slayer`}</h1>}
         <img src={ReactImage} alt="react" />
       </div>
     );
