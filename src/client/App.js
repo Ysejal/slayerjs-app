c0fe2fa1f64838b02a5a92e1601a4d67ae8e4d10
huxunyue <yue19961209@icloud.com>
2021-04-21 16:23:01 +0200

    essai

--- src/client/App.js
+++ src/client/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+// import {Router, Route, hashHistory} from 'react-route';
+import {Button} from 'antd';
+import Header from '../components/header';
+import 'antd/dist/antd.css';
 import './app.css';
-import TurtleImage from './turtle.png';
+import TurtleImage from '../image/turtle.png';
 
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
       <div>
+          <Header></Header>
         {<h1>{`Slayer`}</h1>}
         <img src={TurtleImage} alt="turtle" width="80%" />
       </div>
     );
-  }
+  };
 }
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+
+export default class Index extends React.Component {
+    render() {
+      return (
+        <div>
+          
+        </div>
+      );
+    }
+  }
 
 ReactDOM.render(<App />, document.getElementById('root'));
