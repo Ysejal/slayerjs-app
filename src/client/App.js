a910e3e65bf7815612cdcb037d602060b3c85895
Soufiane Chikar <soufiane.chikar@protonmail.com>
2021-04-22 04:33:46 +0200

    algo de gen  map fait mais probleme canvas

--- src/client/App.js
+++ src/client/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Map from './modules/map.js';
 import './style/main.css';
 
 
@@ -6,7 +7,15 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        {<h1>{`Niveau XX - _Helheim_ - !!Work in progress!!`}</h1>}
+        <header></header>
+        <section>
+          <div class="wrapper">
+            <div id="canvasBox">
+              <canvas id="mapCanvas" width="800" height="2400"></canvas>
+            </div>
+          </div>
+        </section>
+        <footer></footer>
       </div>
     );
   }
